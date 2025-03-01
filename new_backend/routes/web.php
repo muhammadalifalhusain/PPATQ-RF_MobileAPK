7636b0fae6be6918f924b2c8303dff73bfb8f63f
muhammadalifalhusain <muhalifalhusain@gmail.com>
2025-03-01 13:39:52 +0700

    fix : route API kesehatan

--- new_backend/routes/web.php
+++ new_backend/routes/web.php
@@ -2,8 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SantriController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+
 
 Route::get('/santri', [SantriController::class, 'searchSantri']);
 Route::get('/kelas', [SantriController::class, 'getKelas']);
 Route::get('/data-santri', [SantriController::class, 'getSantri']);
+
+Route::get('/kesehatan', function () {
+    $response = Http::get("http://api.ppatq-rf.id/api/kesehatan-santri");
+    return $response->json();
+});
 
